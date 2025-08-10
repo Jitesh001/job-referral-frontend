b44ddf36c934f9993cced3150bdab49154a7e84c
Jitesh Shewale <jitesh@Jiteshs-MacBook-Pro.local>
2025-08-10 12:54:29 +0530

    changed allowed host

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,7 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
-    allowedHosts: [
-      'job-referral-frontend-deployment.up.railway.app',
-    ],
+    allowedHosts: ["job-referral.up.railway.app", "localhost"],
   },
   plugins: [
     vue(),
